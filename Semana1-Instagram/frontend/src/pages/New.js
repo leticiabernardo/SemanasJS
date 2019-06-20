e326c24443c6869bb4d99fe678ded1c809b357c2
Letícia Bernardo <leticiaellenbernardo@gmail.com>
2019-06-19 22:52:51 -0300

    Instagram - Create frontend app

---
+++ Semana1-Instagram/frontend/src/pages/New.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+import './New.css';
+import api from '../services/api';
+
+class New extends Component {
+    state = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: '',
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        console.log(this.state.image);
+        
+        const data = new FormData();
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data);
+
+        this.props.history.push('/');
+    }
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] });
+    };
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    render() {
+        return (
+            <form id="new-post" onSubmit={this.handleSubmit}>
+                <input type="file" onChange={this.handleImageChange} />
+
+                <input 
+                    type="text" 
+                    name="author"
+                    placeholder="Digite o autor do post"
+                    onChange={this.handleChange}
+                    value={this.state.author}
+                />
+
+                <input 
+                    type="text" 
+                    name="place"
+                    placeholder="Digite o local do post"
+                    onChange={this.handleChange}
+                    value={this.state.place}
+                />
+
+                <input 
+                    type="text" 
+                    name="description"
+                    placeholder="Digite sua descrição"
+                    onChange={this.handleChange}
+                    value={this.state.description}
+                />
+
+                <input 
+                    type="text" 
+                    name="hashtags"
+                    placeholder="Digite as hashtags"
+                    onChange={this.handleChange}
+                    value={this.state.hashtags}
+                />
+
+                <button type="submit">Enviar</button>
+                
+            </form>
+        );
+    }
+}
+
+export default New;
