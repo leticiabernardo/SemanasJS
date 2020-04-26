562086f6f604c246125ad1a4329bf489e5088e08
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2020-04-26 19:15:03 -0300

    Rename folder

---
+++ Semana01-Instagram/frontend/src/pages/Feed.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+import io from 'socket.io-client';
+
+import './Feed.css';
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+class Feed extends Component {
+    state = {
+        feed: [],
+
+    };
+
+
+
+    async componentDidMount() {
+        this.registerToSocket();
+
+        const response = await api.get('posts');
+        
+        this.setState({ feed: response.data });
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ... this.state.feed] });
+        });
+
+        socket.on('like', likedPost => {
+            console.log("passou aqui")
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post._id === likedPost._id ? likedPost : post
+                    )
+            })
+        });
+
+    }
+
+    render() {
+        return (
+            <section id="post-list">
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                    <header>
+                        <div className="user-info">
+                            <span>{post.author}</span>
+                            <span className="place">{post.place}</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src={`http://localhost:3333/files/${post.image}`} alt="Foto"/>
+
+                    <footer>
+                        <div className="actions">
+                            <button type="button" onClick={() => this.handleLike(post._id)}>
+                                <img src={like} al=""/>
+                            </button>
+                            <img src={comment} al=""/>
+                            <img src={send} al=""/>
+                        </div>
+
+                        <strong>{post.likes} curtidas</strong>
+
+                        <p>
+                            {post.description}
+                            <span>{post.hashtags}</span>
+                        </p>
+
+                    </footer>
+                </article>
+                
+                ))}
+            </section>
+            
+        );
+    }
+}
+
+export default Feed;
